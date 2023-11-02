cb4008cadec6b895b5fdac35a174aafbaafd174c
Sathyaprakash Narayanan <max.satabiossathya@gmail.com>
2023-11-02 16:45:48 -0700

    updated hardcoded one layer nn

--- src/test.py
+++ src/test.py
@@ -11,38 +11,38 @@ async def test_7seg(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # reset
-    dut._log.info("reset")
-    dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # # reset
+    # dut._log.info("reset")
+    # dut.rst_n.value = 0
+    # # set the compare value
+    # dut.ui_in.value = 1
+    # await ClockCycles(dut.clk, 10)
+    # dut.rst_n.value = 1
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    # # the compare value is shifted 10 bits inside the design to allow slower counting
+    # max_count = dut.ui_in.value << 10
+    # dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
+    # # check all segments and roll over
+    # for i in range(15):
+    #     dut._log.info("check segment {}".format(i))
+    #     await ClockCycles(dut.clk, max_count)
+    #     assert int(dut.segments.value) == segments[i % 10]
 
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
+    #     # all bidirectionals are set to output
+    #     assert dut.uio_oe == 0xFF
 
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # # reset
+    # dut.rst_n.value = 0
+    # # set a different compare value
+    # dut.ui_in.value = 3
+    # await ClockCycles(dut.clk, 10)
+    # dut.rst_n.value = 1
 
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    # max_count = dut.ui_in.value << 10
+    # dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
+    # # check all segments and roll over
+    # for i in range(15):
+    #     dut._log.info("check segment {}".format(i))
+    #     await ClockCycles(dut.clk, max_count)
+    #     assert int(dut.segments.value) == segments[i % 10]
 
--- src/tt_um_perceptron.v
+++ src/tt_um_perceptron.v
@@ -13,34 +13,58 @@ module tt_um_perceptron #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
   
 
-reg [7:0] X [1:0][0];
-reg signed [7:0] W [1:0][0];
+reg [7:0] X [1:0]; // [dimnension of data][no. of samples]
+reg signed [7:0] W [1:0];
 
 reg [7:0] partial_mac_out;
+reg [7:0] z1;
 reg [7:0] mac_out;
 reg [7:0] activation_out;
 
 
 initial begin
 
-    W[0][0] = {4'd0,4'd4};
-    W[1][0] = {4'd0,4'd9};
+    W[0]= {4'd0,4'd4};
+    W[1]= {4'd0,4'd9};
 
-    X[0][0] = {4'd0,4'd2};
-    X[1][0] = {4'd0,4'd3};
+    W[0]= {4'd0,4'd4};
+    W[1] = {4'd0,4'd9};
+
+    X[0] = {4'd0,4'd2};
+    X[1]= {4'd0,4'd3};
+    partial_mac_out = 0;
 
 
 end
 
+
+
 always @(posedge clk)begin
 
-    partial_mac_out = X[0][0] * W[0][0];
-    mac_out = partial_mac_out + X[1][0] * W[1][0];
-    activation_out = mac_out > 8'd0 ? mac_out : 8'd0;
+    partial_mac_out = X[0] * W[0];
+    z1 = partial_mac_out + (X[1] * W[1]);
+    activation_out = z1 > 8'd0 ? z1 : 8'd0;
+
+
+
+    // mac mac_instance
+    // ( 
+    // .x(X[0]),
+    // .w(W[0]),
+    // .previous_out(partial_mac_out),
+    // .clk(clk),
+    // .rst_n(rst_n), // reset_n - low to reset
+    // .out(z1)
+    // );
+
+  
+    // activation_out = mac_out > 8'd0 ? mac_out : 8'd0;
+    $display("%b %b %b",partial_mac_out, z1, activation_out);
 
    
 
 end
 assign  uo_out = activation_out;
+
 
 endmodule
