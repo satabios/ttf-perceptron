820cd4a1eb74e45c02f2e2e6a1877d7297dc0348
Sathyaprakash Narayanan <max.satabiossathya@gmail.com>
2023-11-02 15:45:36 -0700

    first push

---
+++ build-script.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd src/
+make clean
+clear
+make
+cd ../
---
+++ git-push.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+git_push() {
+
+git add .
+git commit -m "$1"
+git push -u origin main
+}
+
+echo "Message to Push?"
+read message
+git_push "$message"
