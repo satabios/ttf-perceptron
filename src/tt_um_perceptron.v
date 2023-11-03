a61dd5b1310895670c07aee23eb8e917eb5c9b50
Sathyaprakash Narayanan <max.satabiossathya@gmail.com>
2023-11-03 15:19:22 -0700

    updated back to basics

--- src/tt_um_perceptron.v
+++ src/tt_um_perceptron.v
@@ -95,25 +95,25 @@ always @(posedge clk)begin
         delta = 0;
     end
     else begin
+
+
     mac_out[0] = X[0][0][3:0] * W[0][3:0] + X[1][0][3:0] * W[1][3:0];
-    activation_out[0] <= mac_out[0] > 8'd0 ? 8'd1 : 8'd0;    // stick to perceptron for now
+    activation_out[0] <= mac_out[0] > 8'b00000000 ? 8'b11111111 : 8'b00000000;    // stick to perceptron for now
     delta <= y_current - activation_out[0];
     W[0] <= W[0] + (delta * X[0][0]);
     W[1] <= W[1] + (delta * X[1][0]);
 
 
     mac_out[1] = X[0][1][3:0] * W[0][3:0] + X[1][1][3:0] * W[1][3:0];
-    activation_out[1] <= mac_out[1] > 8'd0 ? 8'd1 : 8'd0;    // stick to perceptron for now
+    activation_out[1] <= mac_out[1] > 8'b00000000 ? 8'b11111111 : 8'b00000000;    // stick to perceptron for now
     delta <= y_current - activation_out[1];
-    $display("Delta2:%d",delta);
     W[0] <= W[0] + (delta * X[0][1]);
     W[1] <= W[1] + (delta * X[1][1]);
 
 
     mac_out[2] = X[0][2][3:0] * W[0][3:0] + X[1][2][3:0] * W[1][3:0];
-    activation_out[2] <= mac_out[2] > 8'd0 ? 8'd1 : 8'd0;    // stick to perceptron for now
+    activation_out[2] <= mac_out[2] > 8'b00000000 ? 8'b11111111 : 8'b00000000;    // stick to perceptron for now
     delta <= y_current - activation_out[2];
-    $display("Delta3:%d",delta);
     W[0] <= W[0] + (delta * X[0][2]);
     W[1] <= W[1] + (delta * X[1][2]);
 
